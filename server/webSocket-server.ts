f01b884c2f31b5eb1495d93f7d9b9a8bd8f16ab5
iamtheef <eftihis.riise@gmail.com>
2020-07-28 15:47:01 +0300

    message exchanging

--- server/webSocket-server.ts
+++ server/webSocket-server.ts
@@ -2,24 +2,14 @@ import { server } from "./configuration";
 
 export const io = require("socket.io").listen(server);
 
-let connected = [];
-
 export const socketServer = () => {
   io.sockets.on("connection", (socket: any) => {
-    connected.push(socket);
-
-    socket.on("new-message", (msg: string, username: string) => {
-      io.emit("new-message", msg, username);
-      console.log(username, msg);
+    socket.on("incoming", (msg: string, username: string) => {
+      io.emit("new-message", username, msg);
     });
 
-    socket.on("connected");
-
-    socket.broadcast.emit("");
-
-    socket.on("disconnect", (socket: any) => {
-      io.emit("message", "left the chat");
-      connected.splice(connected.indexOf(socket), 1);
+    socket.on("disconnect", () => {
+      io.emit("message", "A user has left the chat");
     });
   });
 };
--- ui/src/Components/ChatWindow.tsx
+++ ui/src/Components/ChatWindow.tsx
@@ -1,21 +1,45 @@
-import React, { FC } from "react";
+import React, { FC, useContext, useState } from "react";
 import io from "socket.io-client";
+import { UserContext } from "../Context/User";
 
 export const ChatWindow: FC = () => {
+  interface Message {
+    username: string;
+    message: string;
+  }
+
+  const { user } = useContext(UserContext);
+  const [messages, setMessages] = useState<Message[]>([]);
+
   const socket = io("http://localhost:4000");
 
-  socket.on("new-message", (msg: string, username: string) => {
-    socket.emit(msg);
-    console.log(msg);
+  socket.on("new-message", (username: string, msg: string) => {
+    setMessages([...messages, { username, message: msg }]);
   });
 
-  socket.on("connected", (username: string) => {
-    socket.emit(username);
-    console.log(username);
+  socket.on("message", (msg: string) => {
+    alert(msg);
   });
+
+  const listenForSubmit = (e: any) => {
+    if (e.keyCode === 13) {
+      socket.emit("incoming", e.target.value, user.username);
+      e.target.value = "";
+    }
+  };
+
   return (
     <div className="chatwin">
-      <input />
+      <ul style={{ listStyleType: "none" }}>
+        {messages.map((msg, i) => (
+          <li key={i} className="message-list">
+            <h4>{msg.username}</h4>
+            <p className="message">{msg.message}</p>
+          </li>
+        ))}
+      </ul>
+
+      <input className="editor" onKeyDown={(e) => listenForSubmit(e)} />
     </div>
   );
 };
