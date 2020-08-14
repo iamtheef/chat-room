c35a2ab290ae57375e48169e3ac6e5cd4c55a9b1
iamtheef <eftihis.riise@gmail.com>
2020-08-14 14:03:14 +0300

    deploying to heroku

---
+++ ui/build/precache-manifest.61d096b2874bc6bcece835d20b45f64d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e5c769d812e1a730d2318e653b86a7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b64097cad2f6a5d3d289",
+    "url": "/static/css/main.4962c664.chunk.css"
+  },
+  {
+    "revision": "57f23f9ad3e8f7bf43c0",
+    "url": "/static/js/2.7cb9bd3c.chunk.js"
+  },
+  {
+    "revision": "79d922c9c27d1fac4746c0511ee8b63c",
+    "url": "/static/js/2.7cb9bd3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b64097cad2f6a5d3d289",
+    "url": "/static/js/main.117cb74e.chunk.js"
+  },
+  {
+    "revision": "1eef1a18fdcab275ebfc",
+    "url": "/static/js/runtime-main.847ad563.js"
+  }
+]);
