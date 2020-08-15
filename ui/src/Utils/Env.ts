955f31a361ca979bd896f4a7e810c4bc10f2329b
iamtheef <eftihis.riise@gmail.com>
2020-08-15 13:32:39 +0300

    logs

--- ui/src/Utils/Env.ts
+++ ui/src/Utils/Env.ts
@@ -2,6 +2,7 @@ let baseUrl = `http://localhost:4000`;
 
 if (process.env.NODE_ENV === "production") {
   baseUrl = process.env.baseURL!;
+  console.log("base url is :: ", baseUrl);
 }
 
 export default baseUrl;
