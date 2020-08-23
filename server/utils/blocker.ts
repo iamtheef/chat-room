d930a126defb8f5ea37028d83f4f05b5d3e4fb17
iamtheef <eftihis.riise@gmail.com>
2020-08-23 12:43:42 +0300

    env client

--- server/utils/blocker.ts
+++ server/utils/blocker.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
 const blocker = (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.origin === process.env.REACT_APP_baseURL) {
+  if (req.headers.origin === process.env.client) {
     next();
   } else {
     res.send("FUCK OFF");
