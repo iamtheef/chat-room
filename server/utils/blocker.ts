9a259bc238a9883e8227544853b4037ae98af18d
iamtheef <eftihis.riise@gmail.com>
2020-08-23 13:21:52 +0300

    debugging blocker

--- server/utils/blocker.ts
+++ server/utils/blocker.ts
@@ -2,7 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 const blocker = (req: Request, res: Response, next: NextFunction) => {
   let origin = req.headers.origin || req.headers.referer;
-  console.log("BLOCKER ", origin);
+  console.log("BLOCKER ", origin, process.env.client);
+  console.log(origin === process.env.client);
   if (origin === process.env.client) {
     next();
   } else {
