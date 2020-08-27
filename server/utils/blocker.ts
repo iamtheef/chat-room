688ad34d7b879e742aa170a77c371f13e09a9873
iamtheef <eftihis.riise@gmail.com>
2020-08-27 21:25:58 +0300

    blocker done

--- server/utils/blocker.ts
+++ server/utils/blocker.ts
@@ -1,14 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 
 const blocker = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.headers);
   let origin = req.headers.referer || req.headers.origin;
 
-  if (origin === process.env.client) {
-    console.log("ACCEPTED");
+  if (origin === process.env.client || !origin) {
     next();
   } else {
-    console.log("REJECTED");
     res.send("FUCK OFF");
   }
 };
