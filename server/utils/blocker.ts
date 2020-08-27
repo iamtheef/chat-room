1815b19f29e591c8002119a27f47785b279ba3b8
iamtheef <eftihis.riise@gmail.com>
2020-08-27 20:43:55 +0300

    reimplementing custom blocker

--- server/utils/blocker.ts
+++ server/utils/blocker.ts
@@ -1,9 +1,9 @@
 import { Request, Response, NextFunction } from "express";
 
 const blocker = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req);
   let origin = req.headers.referer || req.headers.origin;
-  console.log(origin);
-  console.log(process.env.client);
+
   if (origin === process.env.client) {
     console.log("ACCEPTED");
     next();
